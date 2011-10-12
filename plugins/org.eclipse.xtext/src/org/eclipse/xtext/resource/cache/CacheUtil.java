e2ebae18af457f035fc88f7482e1b7d32a194758
Mark Christiaens <mark.christiaens@sigasi.com>
2011-10-12 13:29:25 +0200

    Import cleanup

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/cache/CacheUtil.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/cache/CacheUtil.java
@@ -28,7 +28,6 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
-import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
 import com.google.common.collect.Lists;
